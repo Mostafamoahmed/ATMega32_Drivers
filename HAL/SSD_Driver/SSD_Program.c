8b024eeee3d4eeaaee9749e741b9e6ceea7e5fd8
Mostafamoahmed <mostafamohamed.49211@gmail.com>
2024-01-30 15:11:00 +0200

    SSD

---
+++ HAL/SSD_Driver/SSD_Program.c
@@ -0,0 +1,14 @@
+#include "DIO_interface.h"
+
+#include "SSD_Interface.h"
+#include "SSD_Private.h"
+
+#include "STD_TYPES.h"
+#incldue "BIT_MATH.h"
+
+static u8 Local_SSD_NUMBERS[10] = SSD_Number_ARR ;
+
+void SSD_Init(SSD_Type SSD_Configuration)
+{
+	
+}
