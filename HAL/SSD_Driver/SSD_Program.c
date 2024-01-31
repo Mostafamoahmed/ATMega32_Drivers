20ece2a70f14626c59704ca8e5e847744d75f4ee
Mostafamoahmed <mostafamohamed.49211@gmail.com>
2024-01-31 15:19:26 +0200

    SSD

--- HAL/SSD_Driver/SSD_Program.c
+++ HAL/SSD_Driver/SSD_Program.c
@@ -8,7 +8,51 @@
 
 static u8 Local_SSD_NUMBERS[10] = SSD_Number_ARR ;
 
+//This function set the port connceted to the SSD to OUTPUT PORT 
 void SSD_Init(SSD_Type SSD_Configuration)
 {
-	
+	DIO_ErrorStatus DIO_enumSetPortDirection     ( SSD_Configuration.DataPort, DIO_PORT_OUTPUT );
+}
+
+//This function enable the ssd by enabling the common pin 
+void SSD_Enable(SSD_Type SSD_Configuration)
+{
+	if ( SSD_Configuration.Type == SSD_COMMON_CATHODE )
+	{
+		DIO_ErrorStatus DIO_enumSetPinDirection ( SSD_Configuration.EnablePort, SSD_Configuration.EnablePin , DIO_PIN_OUTPUT );
+		DIO_ErrorStatus DIO_enumSetPinValue		( SSD_Configuration.EnablePort , SSD_Configuration.EnablePin , DIO_PIN_LOW	);
+	}
+	else if ( SSD_Configuration.Type == SSD_COMMON_ANODE )
+	{
+		DIO_ErrorStatus DIO_enumSetPinDirection ( SSD_Configuration.EnablePort, SSD_Configuration.EnablePin , DIO_PIN_OUTPUT );
+		DIO_ErrorStatus DIO_enumSetPinValue		( SSD_Configuration.EnablePort , SSD_Configuration.EnablePin , DIO_PIN_HIGH	);
+	}
+}
+
+//This Function disable the ssd by disabling the common pin
+void SSD_Enable(SSD_Type SSD_Configuration)
+{
+	if ( SSD_Configuration.Type == SSD_COMMON_CATHODE )
+	{
+		DIO_ErrorStatus DIO_enumSetPinDirection ( SSD_Configuration.EnablePort, SSD_Configuration.EnablePin , DIO_PIN_OUTPUT );
+		DIO_ErrorStatus DIO_enumSetPinValue		( SSD_Configuration.EnablePort , SSD_Configuration.EnablePin , DIO_PIN_HIGH	);
+	}
+	else if ( SSD_Configuration.Type == SSD_COMMON_ANODE )
+	{
+		DIO_ErrorStatus DIO_enumSetPinDirection ( SSD_Configuration.EnablePort, SSD_Configuration.EnablePin , DIO_PIN_OUTPUT );
+		DIO_ErrorStatus DIO_enumSetPinValue		( SSD_Configuration.EnablePort , SSD_Configuration.EnablePin , DIO_PIN_LOW	);
+	}
+}
+
+//This function sends a number to the SSD
+void SSD_SendNumber(SSD_Type SSD_Configuration , u8 Number)
+{
+	if (SSD_Configuration.Type == SSD_COMMON_CATHODE)
+	{
+		DIO_ErrorStatus DIO_enumSetPortValue         	(SSD_Configuration.DataPort, Local_SSD_NUMBERS[Number]);
+	}
+	else if (SSD_Configuration.Type == SSD_COMMON_CATHODE)
+	{
+		DIO_ErrorStatus DIO_enumSetPortValue         	(SSD_Configuration.DataPort, ~(Local_SSD_NUMBERS[Number]));
+	}
 }
